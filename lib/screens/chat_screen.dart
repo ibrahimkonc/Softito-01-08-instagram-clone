90fdf9e436ac4fdb0ebad79a86ab5c9c0b1c0cf7
ibrahimkonc <iboknc238@gmail.com>
2023-03-17 10:26:39 +0300

    İmage add

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -27,21 +27,24 @@ class ChatPage extends StatelessWidget {
                   backgroundColor: MaterialStateProperty.all(Colors.black)),
               onPressed: () {},
               child: Row(
-                children: [
-                  Text("ibrahim.konc"),
+                children: const [
+                  Text(
+                    "ibrahim.konc",
+                    style: TextStyle(fontSize: 20),
+                  ),
                   Icon(Icons.keyboard_arrow_down_rounded)
                 ],
               )),
         ),
         actions: [
           CustomIconButton(
-            icon: "assets/icons/favorite.png",
+            icon: "assets/icons/ic_camera.png",
             onPressed: () {},
             height: 25,
             width: 25,
           ),
           CustomIconButton(
-            icon: "assets/icons/message.png",
+            icon: "assets/icons/ic_add.png",
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const ChatPage()));
@@ -96,37 +99,47 @@ class ChatPage extends StatelessWidget {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  GestureDetector(
-                                    onTap: () {
-                                      print("dokundu $index");
-                                    },
-                                    child: Container(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(2.0),
-                                        child: CircleAvatar(
-                                          backgroundColor: Colors.black,
-                                          radius: 42,
+                                  Stack(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          print("dokundu $index");
+                                        },
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(2.0),
                                           child: CircleAvatar(
+                                            backgroundColor: Colors.black,
+                                            radius: 42,
+                                            child: CircleAvatar(
                                               backgroundImage: NetworkImage(
                                                   discoveryProvider
                                                       .users[index].userAvatar
                                                       .toString()),
                                               radius: 48,
-                                              child: Container(
-                                                decoration: BoxDecoration(
-                                                    borderRadius:
-                                                        BorderRadius.all(10)),
-                                                color: Colors.amber,
-                                                padding: EdgeInsets.all(20),
-                                                child: Text(
-                                                  "Text",
-                                                  style: TextStyle(
-                                                      color: Colors.black),
-                                                ),
-                                              )),
+                                            ),
+                                          ),
                                         ),
                                       ),
-                                    ),
+                                      Container(
+                                        margin: EdgeInsets.only(left: 10),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          color:
+                                              Color.fromARGB(239, 58, 58, 58),
+                                        ),
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 5, horizontal: 12),
+                                        child: Text(
+                                          index == 0
+                                              ? "+"
+                                              : discoveryProvider
+                                                  .users[index].username
+                                                  .toString(),
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                   const SizedBox(
                                     height: 5,
@@ -149,6 +162,31 @@ class ChatPage extends StatelessWidget {
                     )
                   ],
                 )),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Mesajlar",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: null,
+                    child: const Text("İstekler",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 54, 81, 255),
+                            fontSize: 18)),
+                  ),
+                ],
+              ),
+            ),
             Flexible(
               child: ListView.builder(
                   physics: NeverScrollableScrollPhysics(),
@@ -164,9 +202,9 @@ class ChatPage extends StatelessWidget {
                               .toString()),
                         ),
                         trailing: const Icon(
-                          Icons.close,
+                          Icons.camera_alt_outlined,
                           color: Colors.grey,
-                          size: 17,
+                          size: 30,
                         ),
                         subtitle: Text(
                             discoveryProvider.users[index].subusername
